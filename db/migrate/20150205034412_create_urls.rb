ef5dabf745709e0249a5eeb58778dc4959a32511
Chandra Carney <chandracarney@me.com>
2015-02-04 22:14:38 -0700

    added fields for urls in db

---
+++ db/migrate/20150205034412_create_urls.rb
@@ -0,0 +1,12 @@
+class CreateUrls < ActiveRecord::Migration
+  def change
+    create_table :urls do |t|
+      t.string :original_url, null: false, default: ''
+      t.string :slug
+      t.string :title
+      t.integer :popularity, default: 0
+
+      t.timestamps null: false
+    end
+  end
+end
