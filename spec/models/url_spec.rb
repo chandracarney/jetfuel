c732adcc06b4285c579ccf23803c175b354f1f7c
Chandra Carney <chandracarney@me.com>
2015-02-08 09:19:37 -0700

    url shortening test added and passong. views also display shortened urls

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,4 +1,9 @@
 require "rails_helper"
 
 RSpec.describe Url, :type => :model do
+  it "can produce shortened urls" do
+    url = Url.create!(original_url: "www.example.com/really/long/url/here")
+
+    expect(url.slug).to eq(url.id.to_s(36))
+  end
 end
