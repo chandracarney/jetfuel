a5cb7f56b0b0d66f5ef8fd398b13801d9721b2c7
Chandra Carney <chandracarney@me.com>
2015-02-08 08:40:38 -0700

    fixed things for hound

--- db/migrate/20150205034412_create_urls.rb
+++ db/migrate/20150205034412_create_urls.rb
@@ -1,7 +1,7 @@
 class CreateUrls < ActiveRecord::Migration
   def change
     create_table :urls do |t|
-      t.string :original_url, null: false, default: ''
+      t.string :original_url, null: false, default: ""
       t.string :slug
       t.string :title
       t.integer :popularity, default: 0
