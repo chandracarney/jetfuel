f6735d3613f0b6da4a2495c1c1a8566e0f85489c
Chandra Carney <chandracarney@me.com>
2015-02-08 09:45:34 -0700

    what is happening on git

---
+++ spec/features/url_listing_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe "url listing page", type: :feature do
+  it "lists urls and shortened urls" do
+    url1 = Url.create!(original_url: "www.first.example.com")
+
+    url2 = Url.create!(original_url: "www.second.example.com")
+    visit root_path
+
+    expect(page).to have_content(url1.original_url)
+    expect(page).to have_content("Shortened URL: #{url1.slug}")
+    expect(page).to have_content(url2.original_url)
+    expect(page).to have_content("Shortened URL: #{url2.slug}")
+  end
+end
