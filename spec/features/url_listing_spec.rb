11de2f0b236b46942f5c7f4d0aa2d6ecf052c847
Chandra Carney <chandracarney@me.com>
2015-02-08 11:04:06 -0700

    can redirect to external original urls

--- spec/features/url_listing_spec.rb
+++ spec/features/url_listing_spec.rb
@@ -8,22 +8,8 @@ describe "url listing page", type: :feature do
     visit root_path
 
     expect(page).to have_content(url1.original_url)
-    expect(page).to have_content("Shortened URL: #{url1.slug}")
+    expect(page).to have_content("Shortened URL: fuel.it/#{url1.slug}")
     expect(page).to have_content(url2.original_url)
-    expect(page).to have_content("Shortened URL: #{url2.slug}")
-  end
-
-  context "url form" do
-    it "can add a url" do
-      visit root_path
-      find_link("Add url").click
-      long_url = "www.example.com/really/long/example/url"
-
-      fill_in("Original url", with: long_url)
-      find_button("Shorten!").click
-
-      expect(current_path).to eq(root_path)
-      expect(page).to have_content(long_url)
-    end
+    expect(page).to have_content("Shortened URL: fuel.it/#{url2.slug}")
   end
 end
--- spec/features/url_shortening_spec.rb
+++ spec/features/url_shortening_spec.rb
@@ -1,1 +1,29 @@
 require "rails_helper"
+
+describe "shortened urls", type: :feature do
+
+  context "url form" do
+    it "can add a url" do
+      visit root_path
+      find_link("Add url").click
+      long_url = "www.example.com/really/long/example/url"
+
+      fill_in("Original url", with: long_url)
+      find_button("Shorten!").click
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content(long_url)
+    end
+  end
+
+  context "url redirect" do
+    it "redirects to original url" do
+      url = Url.create!(original_url: "https://www.example.com/")
+
+      visit urls_path
+      find_link("fuel.it/#{url.slug}").click
+
+      expect(current_url).to eq(url.original_url)
+    end
+  end
+end
